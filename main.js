ef37892f4245370100bc8fbab9a3fa32a41d9f32
Courage <berryflezzyfrosh@gmail.com>
2024-10-17 05:05:17 -1200

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed3 = new Typed('.text', {
+    strings: ['Frontend Developer', 'YouTuber'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    smartBackspace: true, // this is a default
+    loop: true
+});
